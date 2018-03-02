cd6146b6abb043f55743e54d5f4f5aa3c99faeee
JeWallin <johannes.eriksson-wallin@xlent.se>
2018-03-02 11:47:31 +0100

    ITournamnet added

---
+++ Tournament/Tournament/Tournament/ITournament.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tournament.Nodes;
+using System.Collections.Generic;
+
+namespace Tournament.Tournament
+{
+    public interface ITournament
+    {
+        void AddLeafNode(LeafNode node);
+        void GenerateTournamentTree();
+        List<INode> GetActiveTournamnetNodes();
+    }
+}
